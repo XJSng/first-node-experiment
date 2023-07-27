45b1b351c1e3f0eb4b56aee445e7aa717ae2b97f
Xu Jie <sngxujie@gmail.com>
2023-07-27 19:24:16 +0800

    Update intro-to-express comments

--- intro-to-express.js
+++ intro-to-express.js
@@ -1,10 +1,10 @@
 /*
-Express is a pre-written JavaScript code we can leverage so that we don't recreate existing applications
-Think of Node.js as basic lego blocks and Express as new lego block types, specific to building a web server
-To install express we go to npm (supermarket) to install express
-By typing "npm install express" to download the express package
-npm will automatically create a "node_modules" folder with express and its dependencies
-Important to note: the express package will be installed on your terminal's current directory.
+"Express" is a compilation of pre-written JavaScript code we can leverage in recreating new or existing applications
+Think of "Node.js" as basic lego blocks and "Express" as additional lego block types, specific to building a web server
+To install "express", we go to npm (supermarket) in the terminal:
+    By typing "npm install express" to download the "express" package
+    npm will automatically create a "node_modules" folder with "express" and its dependencies
+Important to note: the "express" package will always install on your current terminal current directory
 */
 
 const express = require("express") // Node will look for the express module
