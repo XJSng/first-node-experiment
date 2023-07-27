1c2156707b890403e7a99bb4ebef8d0505165a2d
Xu Jie <sngxujie@gmail.com>
2023-07-27 13:13:18 +0800

    add node test.js code

---
+++ test.js
@@ -0,0 +1,15 @@
+let strawberryCount = 20
+
+if (strawberryCount > 10) {
+    console.log("You have enough strawberries.")
+} else {
+    console.log("You do not have enough berries.")
+}
+
+/*
+Control + ~ :Open Terminal in VS Code
+Type in [node test] in the terminal,
+you should see the return value of "You have enough strawberries."
+
+What we are doing here is passing this test.js file through node.js
+*/
